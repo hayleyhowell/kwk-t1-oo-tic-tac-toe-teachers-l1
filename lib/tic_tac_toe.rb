72e1af8455b9e9358fa7f49f9e752c334a8c40ca
Hayley Howell <hayley_howell@sbcglobal.net>
2018-06-05 03:41:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,5 +57,21 @@ class TicTacToe
     display_board
   end
 
-
+  def turn_count(board)
+    count = 0 
+    board.each do |spot|
+      if spot != " "
+        count += 1 
+      end
+    end
+    return count
+  end
+  
+  def current_player(board)
+    if turn_count(board) % 2 == 0 
+      return "X"
+    else 
+      return "O"
+    end
+  end
 end
