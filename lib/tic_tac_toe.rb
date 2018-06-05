92126a144d7abc64f3d8ea646462f4d9a9d299b5
Hayley Howell <hayley_howell@sbcglobal.net>
2018-06-05 03:33:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,6 +19,6 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 end
